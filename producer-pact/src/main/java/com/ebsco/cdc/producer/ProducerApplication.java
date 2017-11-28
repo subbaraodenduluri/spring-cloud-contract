6067fda837031efdafaa0507a429252b0432c44b
Sanjay Bhupathiraju <sbhupathiraju@ebsco.com>
2017-11-28 13:06:36 -0500

    Pact support added

---
+++ producer-pact/src/main/java/com/ebsco/cdc/producer/ProducerApplication.java
@@ -0,0 +1,19 @@
+package com.ebsco.cdc.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerApplication.class, args);
+    }
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
