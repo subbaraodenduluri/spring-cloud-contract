61c0cac30a3ceb35fc8f204412345b312d4193d4
Sanjay Bhupathiraju <sbhupathiraju@ebsco.com>
2017-11-29 14:07:21 -0500

    cleaned up producer-pact

--- producer-pact/src/main/java/com/ebsco/cdc/producer/service/MessageFormatService.java
+++
@@ -1,8 +0,0 @@
-package com.ebsco.cdc.producer.service;
-
-import com.ebsco.cdc.producer.model.FormattedGreetingDTO;
-import com.ebsco.cdc.producer.model.GreetingDTO;
-
-public interface MessageFormatService {
-    public FormattedGreetingDTO formatGreetingMessage(GreetingDTO greeting);
-}
